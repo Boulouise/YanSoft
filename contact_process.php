da3fabfed9bab4b44595e28c093ceea4e3d2fedb
BOULOUISE <maryamboulouise@gmail.com>
2019-08-29 11:53:24 +0200

    firt Commit

---
+++ contact_process.php
@@ -0,0 +1,39 @@
+<?php
+
+
+use PHPMailer\PHPMailer\PHPMailer;
+
+
+require 'PHPMailer/src/Exception.php';
+require 'PHPMailer/src/PHPMailer.php';
+require 'PHPMailer/src/SMTP.php';
+
+print_r($_POST);
+echo "<br>";
+echo $_POST['email'];
+
+echo "<br>";
+
+$mail = new PHPMailer;
+$mail->isSMTP(); 
+// $mail->SMTPDebug = 2; 
+$mail->Host = "smtp.gmail.com"; 
+$mail->Port = "587"; // typically 587 
+$mail->SMTPSecure = 'tls'; // ssl is depracated
+$mail->SMTPAuth = true;
+$mail->Username = "maryamboulouise@gmail.com";
+$mail->Password = "";
+$mail->setFrom("saidblanchette@gmail.com", "said Blanchette");
+$mail->addAddress($_POST['email'], $_POST['name']);
+$mail->Subject = 'new contact';
+$mail->msgHTML("test body"); // remove if you do not want to send HTML email
+$mail->AltBody = 'HTML not supported';
+
+
+$mail->send();
+ header("Location: index.html");
+// exit();
+ 
+
+?>
+
